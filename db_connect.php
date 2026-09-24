72d8ba624b07ed9dc87cafbb4ad7b4b36c1c75cd
hdsanjose <hanzdarwinsanjose@gmail.com>
2026-09-24 19:12:47 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "webyte";
+$host = getenv('MYSQLHOST') ?: "localhost";
+$user = getenv('MYSQLUSER') ?: "root";
+$password = getenv('MYSQLPASSWORD') ?: "";
+$dbname = getenv('MYSQLDATABASE') ?: "webyte";
+$port = getenv('MYSQLPORT') ?: 3306;
 
-$conn = new mysqli($host, $user, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
