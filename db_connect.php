1a5bbd74e7274e454985215abadb737327df0420
hdsanjose <hanzdarwinsanjose@gmail.com>
2026-09-24 19:41:34 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,14 @@
 <?php
-$host = getenv('MYSQLHOST') ?: "localhost";
-$user = getenv('MYSQLUSER') ?: "root";
-$password = getenv('MYSQLPASSWORD') ?: "";
-$dbname = getenv('MYSQLDATABASE') ?: "webyte";
-$port = getenv('MYSQLPORT') ?: 3306;
+$host = getenv('MYSQLHOST') ?: 'localhost';
+$db   = getenv('MYSQLDATABASE') ?: 'your_local_db';
+$user = getenv('MYSQLUSER') ?: 'root';
+$pass = getenv('MYSQLPASSWORD') ?: '';
+$port = getenv('MYSQLPORT') ?: '3306';
 
 try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (\PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
